68c1cf4c48aa4e0171bc5d948e166585d1a7fb89
Houman <houman@venuscloud.com>
2018-03-03 14:28:14 +0000

    latest SSO (unknown)

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # sso
 An Example with flask and keycloak
 
+####Flask Client
 1) `pip install -U -r requirements.txt`
 2) `python src/app.py`
 3) Login with `foo@bar.com` and `secret`
+
+####KeyCloak Server
+1) Download keycloak
+2) Download latest JRE 1.8
+3) `vim ~/.bash_profile`
+4) `export PATH=/Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin:$PATH`
+5) Restart your Computer
+6) cd keycloak_server/bin/
+7) ./standalone.sh
+
+1) Create the admin under http://localhost:8080/auth
+2) Login http://localhost:8080/auth/admin/
+3) Creating a Realm and User
+https://keycloak.gitbooks.io/documentation/getting_started/topics/first-realm/realm.html
--- src/app.py
+++ src/app.py
@@ -1,13 +1,14 @@
+import json
+
 import flask
-from flask import Flask, render_template
+from flask import Flask, render_template, g
 import flask_login
 from flask_login import login_required
+from flask_oidc import OpenIDConnect
 
 from model.user import User
 
 app = Flask(__name__)
-app.secret_key = 'u\x91\xcf\xfa\x0c\xb9\x95\xe3t\xba2K\x7f\xfd\xca\xa3\x9f\x90\x88\xb8\xee\xa4\xd6\xe4'
-
 
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
@@ -15,68 +16,48 @@ login_manager.init_app(app)
 # Our mock database.
 users = {'foo@bar.com': {'pw': 'secret'}}
 
-
-@login_manager.user_loader
-def user_loader(email):
-    if email not in users:
-        return
-    user = User()
-    user.id = email
-    return user
-
-
-@login_manager.request_loader
-def request_loader(request):
-    email = request.form.get('email')
-    if email not in users:
-        return
-    user = User()
-    user.id = email
-    if request.form['pw'] != users[email]['pw']:
-        return None
-    return user
+app.config.update({
+    'SECRET_KEY': 'u\x91\xcf\xfa\x0c\xb9\x95\xe3t\xba2K\x7f\xfd\xca\xa3\x9f\x90\x88\xb8\xee\xa4\xd6\xe4',
+    'TESTING': True,
+    'DEBUG': True,
+    'OIDC_CLIENT_SECRETS': 'client_secrets.json',
+    'OIDC_ID_TOKEN_COOKIE_SECURE': False,
+    'OIDC_REQUIRE_VERIFIED_EMAIL': False,
+    'OIDC_VALID_ISSUERS': ['http://localhost:8080/auth/realms/demo-realm'],
+    'OIDC_OPENID_REALM': 'http://localhost:5000/oidc_callback'
+})
+oidc = OpenIDConnect(app)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def hello_world():
+    if oidc.user_loggedin:
+        return ('Hello, %s, <a href="/private">See private</a> '
+                '<a href="/logout">Log out</a>') % \
+            oidc.user_getfield('email')
+    else:
+        return 'Welcome anonymous, <a href="/private">Log in</a>'
 
 
-@app.route('/protected')
-@login_required
-def protected():
-    return 'Protected site - Logged in as: ' + flask_login.current_user.id
+@app.route('/private')
+@oidc.require_login
+def hello_me():
+    info = oidc.user_getinfo(['email', 'openid_id'])
+    return ('Hello, %s (%s)! <a href="/">Return</a>' %
+            (info.get('email'), info.get('openid_id')))
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if flask.request.method == 'GET':
-        return '''
-               <form action='login' method='POST'>
-                <input type='text' name='email' id='email' placeholder='email'></input>
-                <input type='password' name='pw' id='pw' placeholder='password'></input>
-                <input type='submit' name='submit'></input>
-               </form>
-               '''
-    email = flask.request.form['email']
-    if email in users and flask.request.form['pw'] == users[email]['pw']:
-        user = User()
-        user.id = email
-        flask_login.login_user(user)
-        return flask.redirect(flask.url_for('protected'))
-    return 'unknown username'
+@app.route('/api')
+@oidc.accept_token(True, ['openid'])
+def hello_api():
+    return json.dumps({'hello': 'Welcome %s' % g.oidc_token_info['sub']})
 
 
 @app.route('/logout')
 def logout():
-    flask_login.logout_user()
-    return 'Logged out'
-
-
-@login_manager.unauthorized_handler
-def unauthorized_handler():
-    return 'Unauthorized'
+    oidc.logout()
+    return 'Hi, you have been logged out! <a href="/">Return</a>'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run('localhost', port=5000)
